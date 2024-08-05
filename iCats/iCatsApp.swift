23bc6f9fb9cac04737a7023e3041d30712d2eaeb
Matias Ortiz Luna <matiasortizluna.contacto@gmail.com>
2024-08-05 17:34:34 +0100

    Initial Commit

---
+++ iCats/iCatsApp.swift
@@ -0,0 +1,32 @@
+//
+//  iCatsApp.swift
+//  iCats
+//
+//  Created by Matias Luna on 05/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iCatsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
